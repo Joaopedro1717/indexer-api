b3889a7e02ccbf5159d75c505f5ced98fe0650ae
Joaopedro1717 <j.pedrominuto@hotmail.com>
2025-04-19 13:37:45 -0300

    indexação feita seguindo os modelos da nossa aplicação

--- app/main.py
+++ app/main.py
@@ -1,49 +1,9 @@
-from fastapi import FastAPI, Depends, Query
-from sqlalchemy.orm import Session
-from pydantic import BaseModel
-from app.core.database import SessionLocal
+from fastapi import FastAPI
+from api import tatuador
+from core.database import engine, Base
 
-from app.services.indexer import index_artist
-from app.schemas.tatto_artist import TattoArtistCreate, TattoArtistRead
-from app.repositories.tatto_artist_repo import create_tatto_artist
-from app.storage.index_storage import inverted_index
+app = FastAPI()
 
-app = FastAPI(
-    title="Tatto indexer API",
-    version="1.0.0"
-)
+Base.metadata.create_all(bind=engine)
 
-def get_db():
-    db = SessionLocal()
-    try:
-        yield db
-    finally:
-        db.close()
-
-class IndexPayload(BaseModel):
-    artist: TattoArtistCreate
-    artist_id: int
-
-
-@app.post("/index")
-def index_endpoint(payload: IndexPayload, db: Session = Depends(get_db)):
-
-    db_artist = create_tatto_artist(db, payload.artist)
-
-    index_artist(payload.artist, db_artist.id)
-
-    return {"message": "Artist created and indexed successfully", "artist_id": db_artist.id}
-
-
-@app.post("/artists/", response_model=TattoArtistRead)
-def create_and_index_artist(
-    artist: TattoArtistCreate,
-    db: Session = Depends(get_db)
-):
-    # Salvar no banco
-    db_artist = create_tatto_artist(db, artist)
-
-    # Indexar automaticamente
-    index_artist(artist, db_artist.id)
-
-    return db_artist
+app.include_router(tatuador.router)
--- app/storage/index_storage.py
+++
@@ -1,3 +0,0 @@
-from collections import defaultdict
-
-inverted_index = defaultdict(set)
